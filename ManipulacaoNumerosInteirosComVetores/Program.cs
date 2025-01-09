f54a845e4834e6cabcfddcf17880eb2729af8c56
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-01-09 00:10:42 -0300

    criacao do codigo

--- ManipulacaoNumerosInteirosComVetores/Program.cs
+++ ManipulacaoNumerosInteirosComVetores/Program.cs
@@ -1,1 +1,41 @@
 ﻿int quantidadeNumeros;
+
+Console.WriteLine("Digite a quantidade de numeros:");
+quantidadeNumeros = int.Parse(Console.ReadLine());
+
+while (quantidadeNumeros <= 0)
+{
+    Console.WriteLine("Número inválido!!");
+    Console.WriteLine("Digite a quantidade de numeros novamente:");
+    quantidadeNumeros = int.Parse(Console.ReadLine());
+}
+
+int[] numeros = new int[quantidadeNumeros];
+
+for (int i = 0; i < quantidadeNumeros; i++)
+{
+    Console.WriteLine("Digite numeros:");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nOs numeros digitados foram:");
+for (int i = 0; i < quantidadeNumeros; i++)
+{
+    Console.WriteLine( numeros[i]);
+}
+
+Console.WriteLine("\nOs numeros digitados na ordem inversa foram:");
+for (int i = quantidadeNumeros - 1; i >= 0; i--)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+int soma;
+
+soma = 0;
+
+for(int i = 0;i < quantidadeNumeros; i++)
+{
+    soma += numeros[i];
+}
+Console.WriteLine("\nA soma dos numeros é: "+ soma);
