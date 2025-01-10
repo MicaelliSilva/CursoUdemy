2f210fb4a61419159eed8c37ea810a84f8fbe97f
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-01-10 15:27:03 -0300

    finalizacao do codigo

--- ManipulacaoNumerosInteirosComVetores/Program.cs
+++ ManipulacaoNumerosInteirosComVetores/Program.cs
@@ -1,12 +1,12 @@
 ﻿int quantidadeNumeros;
 
-Console.WriteLine("Digite a quantidade de numeros:");
+Console.Write("Digite a quantidade de números:");
 quantidadeNumeros = int.Parse(Console.ReadLine());
 
 while (quantidadeNumeros <= 0)
 {
-    Console.WriteLine("Número inválido!!");
-    Console.WriteLine("Digite a quantidade de numeros novamente:");
+    Console.WriteLine("\nNúmero inválido!!");
+    Console.Write("Digite a quantidade de números novamente:");
     quantidadeNumeros = int.Parse(Console.ReadLine());
 }
 
@@ -14,17 +14,17 @@ int[] numeros = new int[quantidadeNumeros];
 
 for (int i = 0; i < quantidadeNumeros; i++)
 {
-    Console.WriteLine("Digite numeros:");
+    Console.Write("\nDigite um número:");
     numeros[i] = int.Parse(Console.ReadLine());
 }
 
-Console.WriteLine("\nOs numeros digitados foram:");
+Console.WriteLine("\nNúmeros digitados:");
 for (int i = 0; i < quantidadeNumeros; i++)
 {
     Console.WriteLine(numeros[i]);
 }
 
-Console.WriteLine("\nOs numeros digitados na ordem inversa foram:");
+Console.WriteLine("\nNúmeros digitados na ordem inversa:");
 for (int i = quantidadeNumeros - 1; i >= 0; i--)
 {
     Console.WriteLine(numeros[i]);
@@ -38,4 +38,75 @@ for (int i = 0; i < quantidadeNumeros; i++)
 {
     soma += numeros[i];
 }
-Console.WriteLine("\nA soma dos numeros é: " + soma);
+Console.WriteLine("\nSoma dos números:" + soma + "\n");
+
+int maiorNumero;
+
+maiorNumero = numeros[0];
+for (int i = 0; i < quantidadeNumeros; i++)
+{
+    if (numeros[i] > maiorNumero)
+    {
+        maiorNumero = numeros[i];
+    }
+
+}
+Console.WriteLine("Maior número digitado:" + maiorNumero);
+
+int menorNumero = numeros[0];
+for (int i = 0; i < quantidadeNumeros; i++)
+{
+    if (numeros[i] < menorNumero)
+    {
+        menorNumero = numeros[i];
+    }
+}
+
+Console.WriteLine("Menor número digitado:" + menorNumero + "\n");
+
+int quantidadePares = 0;
+int quantidadeImpares = 0;
+
+
+for (int i = 0; i < quantidadeNumeros; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        quantidadePares++;
+    }
+    else
+    {
+        quantidadeImpares++;
+    }
+}
+
+Console.WriteLine("Quantidade de números Pares:" + quantidadePares);
+Console.WriteLine("Quantidade de números Ímpares:" + quantidadeImpares + "\n");
+
+int posicaoPares = 0;
+int posicaoImpares = 0;
+
+int[] pares = new int[quantidadePares];
+int[] impares = new int[quantidadeImpares];
+
+for (int i = 0; i < quantidadeNumeros; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        pares[posicaoPares] = numeros[i];
+        posicaoPares++;
+    }
+    else
+    {
+        impares[posicaoImpares] = numeros[i];
+        posicaoImpares++;
+    }
+}
+
+Console.WriteLine("Números Pares digitados:");
+for (int i = 0; i < quantidadePares; i++)
+    Console.WriteLine(pares[i]);
+
+Console.WriteLine("Números Ímpares digitados:");
+for (int i = 0; i < quantidadeImpares; i++)
+    Console.WriteLine(impares[i]);
