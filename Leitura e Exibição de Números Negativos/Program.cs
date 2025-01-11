22986c752e19b052f646333df2335887bf13ba27
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-01-11 16:40:03 -0300

    desenvolvimento do codigo

---
+++ Leitura e Exibição de Números Negativos/Program.cs
@@ -0,0 +1,22 @@
+﻿int n;
+int[] numeros;
+
+Console.Write("Digite a quantidade de números: ");
+n = int.Parse(Console.ReadLine());
+
+numeros = new int[n];
+
+Console.WriteLine("\nDigite os números: ");
+for (int i = 0; i < n; i++)
+{
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nNúmeros negativos digitados:");
+for (int i = 0; i < n; i++)
+{
+    if (numeros[i] < 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
