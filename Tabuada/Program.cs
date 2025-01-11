0f16f0c7521ddfeaa764e8b6f165c9b53770724c
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-01-11 16:25:53 -0300

    desenvolvimento do codigo

---
+++ Tabuada/Program.cs
@@ -0,0 +1,11 @@
+﻿int n;
+int multiplicacao;
+
+Console.Write("Digite o número a ser calculado na tabuada: ");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < 11; i++)
+{
+    multiplicacao = i * n;
+    Console.WriteLine("\n" + i + "x" + n + "=" + multiplicacao);
+}
