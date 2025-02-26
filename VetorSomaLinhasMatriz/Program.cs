dacf44a4a38102674f771d57ca49c391b03bb4bb
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-02-25 23:44:09 -0300

    solucao de exercicio

---
+++ VetorSomaLinhasMatriz/Program.cs
@@ -0,0 +1,36 @@
+﻿int m, n;
+int[,] mat;
+
+Console.WriteLine("Digite o numero de linhas:");
+m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o numero de colunas:");
+n = int.Parse(Console.ReadLine());
+
+mat = new int[m, n];
+
+Console.WriteLine("Digite os numeros da matriz:");
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        mat[i, j] = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+
+int[] vet = new int[m];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        vet[i] += mat[i, j];
+    }
+}
+
+Console.WriteLine("Soma das linhas da matriz:");
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine(vet[i]);
+}
