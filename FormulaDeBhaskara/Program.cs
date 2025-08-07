2af735d2be4ede376ba4b87e9f14e9af293d8f76
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-06 22:26:05 -0300

    exercicio formula de bhaskara

---
+++ FormulaDeBhaskara/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 2;
+int b = 6;
+int c = 4;
+
+double delta = b * b - 4 * a * c;
+double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+Console.WriteLine(delta);
+Console.WriteLine(x1);
+Console.WriteLine(x2);
