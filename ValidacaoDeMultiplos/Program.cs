2b7e65ad3e54cd8c099d15eb6a6b1fdace6f3651
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-06 22:29:04 -0300

    exercicio validacao de multiplos

---
+++ ValidacaoDeMultiplos/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Digite os numeros:");
+
+string[] vet = Console.ReadLine().Split(' ');
+int n1 = int.Parse(vet[0]);
+int n2 = int.Parse(vet[1]);
+
+if (n1 > n2)
+{
+    if (n1 % n2 == 0)
+        Console.WriteLine("São multiplos");
+    else Console.WriteLine("Não são multiplos");
+}
+else if (n2 > n1)
+{
+    if (n2 % n1 == 0)
+        Console.WriteLine("São multiplos");
+    else Console.WriteLine("Não são multiplos");
+}
