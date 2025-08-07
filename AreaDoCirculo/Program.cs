feb6de9bcd1e3ef93423b6e2ab13d89ce6f13047
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-06 22:17:50 -0300

    exercicio area do circulo

---
+++ AreaDoCirculo/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite o valor do raio:");
+
+double raio = double.Parse(Console.ReadLine());
+
+double area;
+double valor_de_pi = 3.14159;
+area = valor_de_pi * (raio * raio);
+
+Console.WriteLine("O valor da area é: " + area.ToString("F4"));
