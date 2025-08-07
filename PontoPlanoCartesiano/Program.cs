c2c21f2107e3331beb305584598e03d673533768
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-07 00:15:52 -0300

    funcao valida maior numero

---
+++ PontoPlanoCartesiano/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("digite o valor de x:");
+double x = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de y:");
+double y = double.Parse(Console.ReadLine());
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("Origem");
+}
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("Quadrante 1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Quadrante 2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Quadrante 3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Quadrante 4");
+}
