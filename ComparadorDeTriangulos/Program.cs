0029a7844124d31b4c6f7a8a9024961ccef13651
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-11 11:39:41 -0300

    atualizando classe

--- ComparadorDeTriangulos/Program.cs
+++ ComparadorDeTriangulos/Program.cs
@@ -13,11 +13,10 @@ Console.WriteLine("Entre com as medidas do triangulo Y:");
  y.Lado2= double.Parse(Console.ReadLine());
  y.Lado3= double.Parse(Console.ReadLine());
 
-double xP = (x.Lado1 + x.Lado2 + x.Lado3) / 2;
-double xarea = Math.Sqrt(xP * (xP - x.Lado1) * (xP - x.Lado2) * (xP - x.Lado3));
 
-double yP = (y.Lado1 + y.Lado2 + y.Lado3) / 2;
-double yarea = Math.Sqrt(yP * (yP - y.Lado1) * (yP - y.Lado2) * (yP - y.Lado3));
+double xarea = x.Area();
+
+double yarea = y.Area();
 
 
 Console.WriteLine("Area de X: " + xarea.ToString("F4"));
--- ComparadorDeTriangulos/Triangulo.cs
+++ ComparadorDeTriangulos/Triangulo.cs
@@ -11,5 +11,12 @@ namespace ComparadorDeTriangulos
         public double Lado1;
         public double Lado2;
         public double Lado3;
+
+        public double Area()
+        {
+            double p = (Lado1 + Lado2 + Lado3) / 2;
+            double raiz = Math.Sqrt(p * (p - Lado1) * (p - Lado2) * (p - Lado3));
+            return raiz;
+        }
     }
 }
