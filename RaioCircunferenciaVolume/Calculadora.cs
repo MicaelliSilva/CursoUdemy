4b35560f30dc6589b9f67d6e861839727b5ebaf9
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-12 16:40:14 -0300

    adicionando exercicio 20

---
+++ RaioCircunferenciaVolume/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿
+namespace RaioCircunferenciaVolume
+{
+    internal class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circuferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ RaioCircunferenciaVolume/Program.cs
@@ -0,0 +1,13 @@
+﻿using RaioCircunferenciaVolume;
+
+Console.Write("Digite o valor do raio:");
+double raio = double.Parse(Console.ReadLine());
+
+double circ = Calculadora.Circuferencia(raio);
+double volume = Calculadora.Volume (raio);
+
+Console.WriteLine("Circuferencia: " + circ.ToString("F2"));
+Console.WriteLine("Volume: " + volume.ToString("F2"));
+
+Console.WriteLine("Valor de pi e: " + Calculadora.Pi.ToString("F2"));
+
