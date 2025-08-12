1ea28ae7f3c557c2dd2124083ad550034bb21f6e
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-11 22:34:06 -0300

    adicionando exercicio 17

---
+++ ControleEstoque/Produto.cs
@@ -0,0 +1,30 @@
+﻿namespace ControleEstoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + "$" +  Preco.ToString("F2") + " , " + Quantidade 
+                + " unidades, Total: $" + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
---
+++ ControleEstoque/Program.cs
@@ -0,0 +1,34 @@
+﻿using ControleEstoque;
+using System.Runtime;
+
+Produto p = new Produto();
+
+Console.WriteLine("Entre com os dados do produto");
+
+Console.Write("Nome: ");
+p.Nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+p.Preco = double.Parse(Console.ReadLine());
+
+Console.Write("Quantidade no estoque: ");
+p.Quantidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Dados produto: " + p);
+
+Console.WriteLine();
+Console.Write("Digite o numero de produtos a ser adicionados: ");
+int qtd = int.Parse(Console.ReadLine());
+p.AdicionarProduto(qtd);
+
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " + p);
+
+Console.WriteLine();
+Console.WriteLine("Digite a quantidade que deseja remover do estoque: ");
+qtd = int.Parse(Console.ReadLine());
+p.RemoverProdutos(qtd);
+
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " + p);
