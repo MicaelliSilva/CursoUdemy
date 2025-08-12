7dce2f32ef6d10838b300b3f85d4245ffb0c36f1
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-12 10:51:21 -0300

    adicionando exercicio 18

---
+++ CalculoRetangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using CalculoRetangulo;
+
+Retangulo r = new Retangulo();
+
+Console.WriteLine("Digite o valor da largura: ");
+r.Largura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da altura: ");
+r.Altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("A Area e: " + r.Area(r.Altura,r.Largura));
+
+Console.WriteLine("O Perimetro e: " + r.Perimetro(r.Altura, r.Largura));
+
+Console.WriteLine("A diagonal e: "+ r.Diagonal(r.Altura,r.Largura));
+
---
+++ CalculoRetangulo/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿namespace CalculoRetangulo
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area(double altura, double largura)
+        {
+            double area = altura * largura;
+            return area;
+        }
+
+        public double Perimetro(double altura, double largura)
+        {
+            double perimetro = altura + altura + largura + largura;
+            return perimetro;
+        }
+
+        public double Diagonal(double altura, double largura)
+        {
+            double soma = (altura * altura) + (largura * largura);
+            double diagonal = Math.Sqrt(soma);
+            return diagonal;
+        }
+
+
+
+
+    }
+}
