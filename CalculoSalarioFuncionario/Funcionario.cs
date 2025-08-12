3b9d32b9aa6c62338f61e6eb78a99ba726342417
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-12 14:04:22 -0300

    adicionando exercicio 19

---
+++ CalculoSalarioFuncionario/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculoSalarioFuncionario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido(double salarioBruto, double imposto)
+        {
+            double salarioLiquido = salarioBruto - imposto;
+            return salarioLiquido;
+        }
+
+        public double AumentarSalario(double porcentagem, double salarioBruto)
+        {
+            porcentagem = porcentagem / 100;
+            double aumentarSalario = (porcentagem * salarioBruto) + salarioBruto;
+            return aumentarSalario;
+        }
+
+    }
+}
---
+++ CalculoSalarioFuncionario/Program.cs
@@ -0,0 +1,24 @@
+﻿using CalculoSalarioFuncionario;
+
+Funcionario f = new Funcionario();
+
+Console.Write("Digite o nome do funcionario: ");
+f.Nome = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Digite o salario bruto: ");
+f.SalarioBruto = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("Digite o imposto: ");
+f.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Salario liquido: "
+    + f.SalarioLiquido(f.SalarioBruto, f.Imposto).ToString("F2"));
+
+Console.WriteLine();
+Console.WriteLine("Digite a porcentagem para aumentar salario: ");
+double porcentagem = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados atualizados: " + f.Nome + ", " + f.AumentarSalario(porcentagem, f.SalarioBruto));
