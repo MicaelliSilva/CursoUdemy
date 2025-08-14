b0c172ab2fc131f05a64abe35aa8a6b2905af837
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-13 22:00:48 -0300

    Projeto banco simples

---
+++ BancoSimples/ContaBancaria.cs
@@ -0,0 +1,20 @@
+﻿namespace BancoSimples
+{
+    internal class ContaBancaria
+    {
+        public string NumeroConta { get; set; }
+        public string NomeTitular { get; set; }
+        public decimal Saldo { get; set; }
+
+        public void Depositar(decimal deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Sacar(decimal saque)
+        {
+            Saldo -= 5 + saque;
+        }
+
+    }
+}
---
+++ BancoSimples/Program.cs
@@ -0,0 +1,54 @@
+﻿using BancoSimples;
+ContaBancaria conta = new ContaBancaria();
+
+Console.Write("Entre com o numero da conta: ");
+string numeroConta = Console.ReadLine();
+
+Console.Write("\nEntre com o titular: ");
+string nometitular = Console.ReadLine();
+
+Console.Write("\nHavera deposito inicial? (S/N): ");
+char opcao = char.Parse(Console.ReadLine());
+if (opcao == 'S')
+{
+    Console.Write("\nEntre com o deposito inicial: ");
+    decimal depositoInicial = decimal.Parse(Console.ReadLine());
+
+    conta = new ContaBancaria()
+    {
+        NumeroConta = numeroConta,
+        Saldo = depositoInicial,
+        NomeTitular = nometitular
+    };
+}
+else
+{
+    conta = new ContaBancaria()
+    {
+        NumeroConta = numeroConta,
+        Saldo = 0,
+        NomeTitular = nometitular
+    };
+}
+
+Console.WriteLine("\nDados da conta: ");
+Console.WriteLine("Conta: " + conta.NumeroConta + " Titular: "
+    + conta.NomeTitular + ", Saldo: $" + conta.Saldo );
+
+Console.Write("\nEntre com um valor para o deposito: ");
+decimal deposito = decimal.Parse(Console.ReadLine());
+
+conta.Depositar(deposito);
+
+Console.WriteLine("\nDados da conta: ");
+Console.WriteLine("Conta: " + conta.NumeroConta + " Titular: "
+    + conta.NomeTitular + ", Saldo: $" + conta.Saldo);
+
+Console.Write("\nEntre um valor para saque: ");
+decimal saque = decimal.Parse(Console.ReadLine());
+
+conta.Sacar(saque);
+
+Console.WriteLine("\nDados da conta: ");
+Console.WriteLine("Conta: " + conta.NumeroConta + " Titular: "
+    + conta.NomeTitular + ", Saldo: $" + conta.Saldo);
