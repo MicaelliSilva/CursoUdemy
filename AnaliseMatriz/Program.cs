78abc5fdb31d0607ff1d767759a781cb180cfeda
Micaelli Silva <micaelli.silva2017@gmail.com>
2025-08-16 00:02:42 -0300

    Exercicio basico matriz

---
+++ AnaliseMatriz/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Digite o tamanho da matriz: ");
+int tamanho = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[tamanho,tamanho];
+
+Console.WriteLine("\nDigite os valores da matriz: ");
+for (int i = 0; i < tamanho; i++)
+{
+    for(int j = 0; j < tamanho; j++)
+    {
+        mat[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nMatriz: ");
+for(int i = 0;i < tamanho; i++)
+{
+   for(int j = 0;j < tamanho; j++)
+    {
+        Console.Write(mat[i,j] + " ");
+        
+    }
+    Console.Write("\n");
+}
+
+Console.WriteLine("\nDiagonal Principal: ");
+for(int i = 0; i < tamanho; i++)
+{
+    Console.Write(mat[i, i] + " ");
+}
+
+int negativos = 0;
+
+for(int i = 0; i < tamanho ; i++)
+{
+    for(int j = 0;j < tamanho; j++)
+    {
+        if (mat[i,j] < 0)
+        {
+            negativos++;
+
+        }
+    }
+}
+
+Console.WriteLine("\n\nQuantidade de negativos: " + negativos);
