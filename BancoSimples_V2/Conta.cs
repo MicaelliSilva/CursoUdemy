b92513c5939248435ce606e42061af9cdad0c52d
Micaelli Silva <micaelli.silva2017@gmail.com>
2026-02-10 17:20:04 -0300

    Atualizacao: nova versão seguindo o enunciado do exercício

---
+++ BancoSimples_V2/Conta.cs
@@ -0,0 +1,42 @@
+﻿namespace BancoSimples
+{
+    internal class Conta
+    {
+        static Decimal Taxa = 5.00m;
+
+        public string Nome { get; set; }
+        public int Numero { get; private set; }
+
+        public decimal Saldo { get; private set; }
+
+        public Conta(string nome, int numero)
+        {
+            Nome = nome;
+            Numero = numero;
+        }
+
+        public Conta(string nome, int numero, decimal depositoInicial) : this(nome, numero)
+        {
+            EfetuarDeposito(depositoInicial);
+        }
+
+        public decimal EfetuarDeposito(decimal deposito)
+        {
+            if (deposito > 0)
+            {
+                Saldo += deposito;
+            }
+
+            return Saldo;
+        }
+
+        public decimal EfetuarSaque(decimal saque)
+        {
+            Saldo -= saque;
+
+            Saldo -= Taxa;
+
+            return Saldo;
+        }
+    }
+}
---
+++ BancoSimples_V2/Program.cs
@@ -0,0 +1,53 @@
+﻿using BancoSimples;
+
+Conta conta;
+
+Console.Write("Entre com o numero da conta: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.Write("\nEntre com o nome do titular da conta: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("\nHavera deposito inicial (s/n)?");
+char opcao = char.Parse(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("\nDigite a opção novamente:");
+    opcao = char.Parse(Console.ReadLine());
+
+    if (opcao == 's')
+    {
+        Console.Write("\nDigite o valor do deposito:");
+        decimal depositoInicial = decimal.Parse(Console.ReadLine());
+
+        conta = new Conta(nome, numero, depositoInicial);
+    }
+    else
+    {
+        conta = new Conta(nome, numero);
+    }
+
+} while (opcao != 's' && opcao != 'n');
+
+Console.WriteLine("\nDados da conta:");
+Console.WriteLine("Conta " + numero + ", Titular: " + nome +
+    ", Saldo: $" + conta.Saldo.ToString("F2"));
+
+Console.Write("\nEntre com um valor para deposito:");
+decimal deposito = decimal.Parse(Console.ReadLine());
+
+conta.EfetuarDeposito(deposito);
+
+Console.WriteLine("\nDados da conta atualizados:");
+Console.WriteLine("Conta " + numero + ", Titular: " + nome +
+    ", Saldo: $" + conta.Saldo.ToString("F2"));
+
+Console.Write("\nEntre com um valor para saque:");
+decimal saque = decimal.Parse(Console.ReadLine());
+
+conta.EfetuarSaque(saque);
+
+Console.WriteLine("\nDados da conta atualizados:");
+Console.WriteLine("Conta " + numero + ", Titular: " + nome +
+    ", Saldo: $" + conta.Saldo.ToString("F2"));
