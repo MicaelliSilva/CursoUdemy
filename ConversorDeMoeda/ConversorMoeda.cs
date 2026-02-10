5ef39adfbbf3e31973a8d96b8829f8743ac384c1
Micaelli Silva <micaelli.silva2017@gmail.com>
2026-02-10 14:22:05 -0300

    resolucao do exercicio 26

---
+++ ConversorDeMoeda/ConversorMoeda.cs
@@ -0,0 +1,18 @@
+﻿namespace ConversorDeMoeda
+{
+    internal class ConversorMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double ConversorDolar(double valorDolar, double dolarComprado)
+        {
+            double calculoDolar = valorDolar * dolarComprado;
+
+            double calculoImposto = calculoDolar * Iof;
+
+            double valorTotal = calculoDolar + calculoImposto;
+
+            return valorTotal;
+        }
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConversorDeMoeda;
+
+Console.Write("Digite o valor do dolar:");
+double valorDolar = double.Parse(Console.ReadLine());
+
+Console.Write("\nDigite o valor que deseja comprar:");
+double dolarComprado = double.Parse(Console.ReadLine());
+
+double conversao = ConversorMoeda.ConversorDolar(valorDolar, dolarComprado);
+
+Console.WriteLine("\nValor a ser pago em reais: " + conversao.ToString("F2"));
